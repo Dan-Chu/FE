1847033be787f8a5c12dd4290295852018665a3b
Jeong mokjin <jungmokjin@gmail.com>
2025-08-22 21:44:42 +0900

    거리 없을 때 api 추가

--- src/pages/Store/StoreList.jsx
+++ src/pages/Store/StoreList.jsx
@@ -122,6 +122,12 @@ export default function StoreList() {
           );
           setData(result);
         }
+      } else {
+        result = await NoneDistanceListGet(
+          page - 1,
+          dataSize
+        );
+        setData(result);
       }
       setLoading(false);
       setMaxPage(result.totalPages);
