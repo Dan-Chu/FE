420336d9a3b694e47a9ca3bb1c7a4104b39e7e24
Jeong mokjin <jungmokjin@gmail.com>
2025-08-24 11:51:46 +0900

    스플래시 그라데이션

--- src/pages/Login/Login2.jsx
+++ src/pages/Login/Login2.jsx
@@ -1,32 +1,47 @@
 import styled from "styled-components";
 import MapLogo from "../../assets/logos/login2_logo.svg?react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { motion as Motion, AnimatePresence } from "framer-motion";
 
 export default function Login2() {
-  const navigate=useNavigate();
+  const navigate = useNavigate();
+  const [isVisible, setIsVisible] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      navigate("/main");
+      setIsVisible(false);
     }, 2000); // 2초 후 사라짐
     return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 정리
   }, [navigate]);
 
   return (
-    <Page>
-      <TextBox>
-        <Text1>
-          <DotText>성</DotText>
-          <span style={{ color: "#CE4927" }}>북구</span>의 숨은 맛집,
-        </Text1>
-        <Text2>
-          <span style={{ color: "#CE4927" }}>단추</span>가<br />
-          찾아줄게요!
-        </Text2>
-      </TextBox>
-      <MapLogo />
-    </Page>
+    <AnimatePresence>
+      {isVisible && (
+        <Motion.div
+          initial={{ opacity: 1 }}
+          animate={{ opacity: 0 }}
+          exit={{ opacity: 0 }}
+          onAnimationComplete={() => navigate("/main")}
+          transition={{ duration: 1.5, ease: "easeInOut" }}
+          className="fixed inset-0 flex items-center justify-center bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 z-50"
+        >
+          <Page>
+            <TextBox>
+              <Text1>
+                <DotText>성</DotText>
+                <span style={{ color: "#CE4927" }}>북구</span>의 숨은 맛집,
+              </Text1>
+              <Text2>
+                <span style={{ color: "#CE4927" }}>단추</span>가<br />
+                찾아줄게요!
+              </Text2>
+            </TextBox>
+            <MapLogo />
+          </Page>
+        </Motion.div>
+      )}
+    </AnimatePresence>
   );
 }
 
